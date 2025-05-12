1abef94a7e13d99d68e0bed1ce1f3aaddbed34ea
stebi13 <andreas@services5.ch>
2025-05-12 12:29:59 +0200

    Update Start.py

--- Start.py
+++ Start.py
@@ -9,14 +9,14 @@ st.markdown('''Die clevere Noten-App für BMLD-Studierende!
 Behalte jederzeit den Überblick über deine Leistungen: Der Notenrechner zeigt dir alle bisherigen Noten, berechnet deinen aktuellen Gesamtschnitt sowie die Durchschnittswerte je Modulgruppe - natürlich inklusive ECTS-Gewichtung.
 ''')
 
-st.write('''Features:   
+display(HTML('Features:   
             <ul>
                 <li>Übersichtliche Darstellung aller Noten</li>
                 <li>Automatische Berechnung des Gesamtdurchschnitts</li>
                 <li>Durchschnittswerte pro Modulgruppe (ECTS-basiert)</li>   
                 <li>Wunschnotenrechner (in Planung): Finde heraus, welche Noten du brauchst, um dein Wunschziel zu erreichen!</li>
             </ul>
-         ''')
+         '))
 
 # st.write('''Diese App wurde von folgenden Studierenden der ZHAW LSFM entwickelt:
 # - **Alessia Molignini**
