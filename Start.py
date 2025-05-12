b7fea359227e94cb1229371d24ab587f409f74d0
stebi13 <andreas@services5.ch>
2025-05-12 12:35:40 +0200

    Update Start.py

--- Start.py
+++ Start.py
@@ -1,16 +1,19 @@
 import streamlit as st
+from functions.notenrechner import display_sidebar
 
 st.title("Notenrechner - kestmo V2")
 st.sidebar.page_link('Start.py', label='Startseite')
 st.sidebar.page_link('pages/1_Dashboard.py', label='Dashboard')
 st.sidebar.page_link('pages/2_Modulgruppen-Uebersicht.py', label='Modulgruppen-Uebersicht')
 
+display_sidebar()
+
 st.markdown('''Die clevere Noten-App für BMLD-Studierende!
 Behalte jederzeit den Überblick über deine Leistungen: Der Notenrechner zeigt dir alle bisherigen Noten, berechnet deinen aktuellen Gesamtschnitt sowie die Durchschnittswerte je Modulgruppe - natürlich inklusive ECTS-Gewichtung.
 ''')
 
 st.html('''<strong>Features:</strong>
-            <ul style="margin-left: 0px;">
+            <ul style="margin-left: 18px;">
                 <li>Übersichtliche Darstellung aller Noten</li>
                 <li>Automatische Berechnung des Gesamtdurchschnitts</li>
                 <li>Durchschnittswerte pro Modulgruppe (ECTS-basiert)</li>   
