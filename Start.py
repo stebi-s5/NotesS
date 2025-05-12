b6c6cbcda5b0f54443d668eca8a2520b274648a8
stebi13 <andreas@services5.ch>
2025-05-12 12:25:17 +0200

    Update Start.py

--- Start.py
+++ Start.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("Notenrechner - kestmo")
+st.title("Notenrechner - kestmo V2")
 st.sidebar.page_link('Start.py', label='Startseite')
 st.sidebar.page_link('pages/1_Dashboard.py', label='Dashboard')
 st.sidebar.page_link('pages/2_Modulgruppen-Uebersicht.py', label='Modulgruppen-Uebersicht')
@@ -9,13 +9,14 @@ st.markdown('''Die clevere Noten-App für BMLD-Studierende!
 Behalte jederzeit den Überblick über deine Leistungen: Der Notenrechner zeigt dir alle bisherigen Noten, berechnet deinen aktuellen Gesamtschnitt sowie die Durchschnittswerte je Modulgruppe – natürlich inklusive ECTS-Gewichtung.
 
 Features:   
-✔️ Übersichtliche Darstellung aller Noten   
-✔️ Automatische Berechnung des Gesamtdurchschnitts   
-✔️ Durchschnittswerte pro Modulgruppe (ECTS-basiert)   
-✔️ Wunschnotenrechner (in Planung): Finde heraus, welche Noten du brauchst, um dein Wunschziel zu erreichen!''')
+<ul>
+    <li>Übersichtliche Darstellung aller Noten</li>
+    <li>Automatische Berechnung des Gesamtdurchschnitts</li>
+    <li>Durchschnittswerte pro Modulgruppe (ECTS-basiert)</li>   
+    <li>Wunschnotenrechner (in Planung): Finde heraus, welche Noten du brauchst, um dein Wunschziel zu erreichen!</li></ul>''')
 
-#st.write('''Diese App wurde von folgenden Studierenden der ZHAW LSFM entwickelt:
-#- **Alessia Molignini**
-#- **Noëlle Keel**
-#- **Sara Stettler**
-#''')
+# st.write('''Diese App wurde von folgenden Studierenden der ZHAW LSFM entwickelt:
+# - **Alessia Molignini**
+# - **Noëlle Keel**
+# - **Sara Stettler**
+ ''')
