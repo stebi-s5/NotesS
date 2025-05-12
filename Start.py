4b3c1438d6d5d01b2cb912f1d1f52fd282f84eae
stebi13 <andreas@services5.ch>
2025-05-12 13:50:11 +0200

    Update Start.py

--- Start.py
+++ Start.py
@@ -1,13 +1,9 @@
 import streamlit as st
-#import pandas as pd
-#from functions.notenrechner import display_sidebar
 
 st.title("Notenrechner - V2")
 st.sidebar.page_link('Start.py', label='Startseite')
 st.sidebar.page_link('pages/1_Dashboard.py', label='Dashboard')
 st.sidebar.page_link('pages/2_Modulgruppen-Uebersicht.py', label='Modulgruppen-Uebersicht')
-
-#display_sidebar()
 
 st.markdown('''Die clevere Noten-App für BMLD-Studierende!
 Behalte jederzeit den Überblick über deine Leistungen: Der Notenrechner zeigt dir alle bisherigen Noten, berechnet deinen aktuellen Gesamtschnitt sowie die Durchschnittswerte je Modulgruppe - natürlich inklusive ECTS-Gewichtung.
